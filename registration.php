3435ffa6706f97ebd84224574778818bf75c9e22
Michał Zymeła <kontakt@michalzymela.com>
2020-05-21 08:39:08 +0200

    module init

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Alpine_Maintenance
+ *
+ * @copyright   Copyright (c) 2020 Alpine Consulting, Inc (www.alpineinc.com)
+ * @author      Michal Zymela (mzymela@alpineinc.com)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Alpine_Maintenance',
+    __DIR__
+);
