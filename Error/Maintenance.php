9468491ea7253f13f0742eb602c10c55a81f0718
Felix Hamader <hamader@e-conomix.at>
2022-11-14 14:55:43 +0100

    :sparkles: [WP-46] Add support for more template Files

--- Error/Maintenance.php
+++ Error/Maintenance.php
@@ -15,6 +15,8 @@ use Magento\Framework\Filesystem;
 use Magento\Framework\Filesystem\Io\File as FileReader;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Phrase;
+use Magento\Store\Model\StoreManager;
+use Magento\Framework\Filesystem\Driver\File;
 
 class Maintenance
 {
@@ -33,21 +35,34 @@ class Maintenance
      * @var FileReader
      */
     private $fileReader;
+    /**
+     * @var StoreManager
+     */
+    private $storeManager;
+    /**
+     * @var File
+     */
+    private $fileDriver;
 
     /**
      * Maintenance constructor.
-     *
      * @param Filesystem $filesystem
      * @param FileReader $fileReader
+     * @param StoreManager $storeManager
+     * @param File $fileDriver
      */
     public function __construct(
         Filesystem $filesystem,
-        FileReader $fileReader
+        FileReader $fileReader,
+        StoreManager $storeManager,
+        File $fileDriver
     ) {
         $this->filesystem = $filesystem;
         $this->fileReader = $fileReader;
         $objectManagerFactory = Bootstrap::createObjectManagerFactory(BP, $_SERVER);
         $this->objectManager = $objectManagerFactory->create($_SERVER);
+        $this->storeManager = $storeManager;
+        $this->fileDriver = $fileDriver;
     }
 
     /**
@@ -69,11 +84,25 @@ class Maintenance
      */
     private function setTemplate()
     {
-        $templatePath = $this->filesystem
-            ->getDirectoryRead(DirectoryList::PUB)
-            ->getAbsolutePath('maintenance/index.html');
+        $path = $this->filesystem->getDirectoryRead(DirectoryList::PUB)->getAbsolutePath() . "maintenance/";
+        $store = $this->storeManager->getStore();
 
-        $template = $this->fileReader->read($templatePath);
+        // Default template ([store_code].html)
+        $template = $path . $store->getCode() . ".html";
+        if (!$this->fileDriver->isExists($template)) {
+            // Second template ([website_code].html)
+            $template = $path . $store->getWebsite()->getCode() . ".html";
+            if (!$this->fileDriver->isExists($template)) {
+                // Third template (fallback.html)
+                $template = $path . "fallback.html";
+                if (!$this->fileDriver->isExists($template)) {
+                    // Last template (index.html)
+                    $template = $path . "index.html";
+                }
+            }
+        }
+
+        $template = $this->fileReader->read($template);
 
         if (!$template) {
             return __('Maintenance html file not found. Upload your index.html file under pub/maintenance directory.');
